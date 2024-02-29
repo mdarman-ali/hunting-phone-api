9d1e7fb441b134327377580a40064a6eac03805f
Md Arman Ali <www.abraryasir76@gmail.com>
2024-02-29 13:09:42 +0600

    Complete

--- js/phone.js
+++ js/phone.js
@@ -1,4 +1,4 @@
-const loadPhone = async (searchText, isShowAll) =>{
+const loadPhone = async (searchText = 'A' , isShowAll) =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
